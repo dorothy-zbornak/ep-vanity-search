8458636d93cb1d856d18061e1d9ca7e2aef1deef
Lawrence Forman <me@merklejerk.com>
2021-10-06 14:25:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('colors');
+const crypto = require('crypto');
+const ethjs = require('ethereumjs-util');
+
+const TARGET_PREFIX = '0xdef1beef';
+
+while (true) {
+    const key = crypto.randomBytes(32);
+    const addr = ethjs.privateToAddress(key);
+    const deployed = ethjs.bufferToHex(ethjs.rlphash([addr, 0]).slice(12));
+    if (deployed.startsWith(TARGET_PREFIX)) {
+        console.log(`deployer: ${ethjs.bufferToHex(addr)}, key: ${ethjs.bufferToHex(key)}, deployed: ${deployed}`);
+        break;
+    }
+}
