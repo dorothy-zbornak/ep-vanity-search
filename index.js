9225ad35961d4625c5e8e9bc396ed792c1c0a244
Lawrence Forman <me@merklejerk.com>
2021-10-06 16:28:52 -0400

    use regex

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('colors');
 const crypto = require('crypto');
 const ethjs = require('ethereumjs-util');
 
-const TARGET_PREFIX = '0xdef1c0de250';
+const TARGET_PREFIX_TEST = /^0xdef1520[a-f]/i;
 
 while (true) {
     const key = crypto.randomBytes(32);
     const addr = ethjs.privateToAddress(key);
     const deployed = ethjs.bufferToHex(ethjs.rlphash([addr, 0]).slice(12));
-    if (deployed.startsWith(TARGET_PREFIX)) {
+    if (TARGET_PREFIX_TEST.test(deployed)) {
         console.log(`deployer: ${ethjs.bufferToHex(addr)}, key: ${ethjs.bufferToHex(key)}, deployed: ${deployed}`);
         break;
     }
