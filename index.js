ea99c2fcbb9a9211c08c7a1b97b08d79c0a55bd3
Lawrence Forman <me@merklejerk.com>
2022-07-03 08:44:02 -0400

    python

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ require('colors');
 const crypto = require('crypto');
 const ethjs = require('ethereumjs-util');
 
-const TARGET_PREFIX_TEST = /^0xdef1520[a-f]/i;
+const TARGET_TESTS = [
+    /^0xec20[a-f]$/i,
+    /^0xec721[a-f]$/i,
+];
 
 while (true) {
     const key = crypto.randomBytes(32);
     const addr = ethjs.privateToAddress(key);
     const deployed = ethjs.bufferToHex(ethjs.rlphash([addr, 0]).slice(12));
-    if (TARGET_PREFIX_TEST.test(deployed)) {
+    if (TARGET_TESTS.some(t => t.test(deployed))) {
         console.log(`deployer: ${ethjs.bufferToHex(addr)}, key: ${ethjs.bufferToHex(key)}, deployed: ${deployed}`);
-        break;
     }
 }
